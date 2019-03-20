37c070066fbc2d799374cfa01bb0081598ad71ef
Marco Loschiavo <marcolos04@gmail.com>
2019-03-20 11:27:58 +0100

    committone

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-// Per while
-import java.util.Set;
-import java.util.Iterator;
-import java.util.Map.Entry;
+
 
 public class Main {
     // Legge dal testo e toglie caratteri speciali ( ();:,. ). Inoltre imposta tutto in Lowercase
@@ -25,12 +23,8 @@ public class Main {
                     filestring[i] = Character.toLowerCase(filestring[i]);
                 }
             }
-
-            System.out.println(filestring);
             return filestring;
-
         }
-
         catch (IOException e) {
             System.out.println(e);
             System.exit(1);
@@ -39,25 +33,98 @@ public class Main {
     }
 
 
-    public static void main(String[] args) {
-
-        char[] text = readTextFromFile();
+    public static void startSequential(char[] text, int n_grams){
         long start, end;
-
         start = System.currentTimeMillis();
-        HashMap hmap = Sequential.computeNGrams(3, text);
+        HashMap hmap = Sequential.computeNGrams(n_grams, text);
         end = System.currentTimeMillis();
 
-         Set set = hmap.entrySet();
+        /* Set set = hmap.entrySet();
             Iterator iterator = set.iterator();
 
             while (iterator.hasNext()) {
                 Entry map_entry = (Entry) iterator.next();
                 System.out.print("key: " + map_entry.getKey() + " , value: ");
                 System.out.println(map_entry.getValue());
-            }
+            }*/
 
         System.out.println(end - start);
 
+    }
+
+    // PER PARALLEL
+    public static ConcurrentHashMap<String, Integer> HashMerge(ConcurrentHashMap<String, Integer> n_grams, ConcurrentHashMap<String, Integer> finalNgrams) {
+        for (ConcurrentHashMap.Entry<String, Integer> entry : n_grams.entrySet()) {
+            int newValue = entry.getValue();
+            Integer existingValue = finalNgrams.get(entry.getKey());
+            if (existingValue != null) {
+                newValue = newValue + existingValue;
+            }
+            finalNgrams.put(entry.getKey(), newValue);
+        }
+        return finalNgrams;
+    }
+
+    public static void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void startParallel(char[] text, int n){
+        int fileLen = text.length;
+        //int threads = Runtime.getRuntime().availableProcessors();
+        int realThreads = 2;
+
+        ConcurrentHashMap<String, Integer> finalNgrams = new ConcurrentHashMap();
+        ArrayList<Future> futuresArray = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(realThreads);
+
+        long start, end;
+        start = System.currentTimeMillis();
+
+
+        double k = Math.floor(fileLen/realThreads);
+        for (int i = 0; i < realThreads; i++) {
+            Future f = executor.submit(new Parallel("t" + i, i * k, ((i+1) * k) + (n - 1) - 1, n, text));
+            futuresArray.add(f);
+        }
+        try{
+            for (Future <ConcurrentHashMap<String, Integer>> f : futuresArray) {
+                ConcurrentHashMap<String, Integer> n_grams = f.get();
+                HashMerge(n_grams,finalNgrams);
+            }
+            awaitTerminationAfterShutdown(executor);
+            //System.out.println("Finished all threads");
+            end = System.currentTimeMillis();
+
+            /*Set set = finalNgrams.entrySet();
+            Iterator iterator = set.iterator();
+            while(iterator.hasNext()) {
+                Map.Entry map_entry = (Map.Entry)iterator.next();
+                System.out.print("key: "+ map_entry.getKey() + " , value: ");
+                System.out.println(map_entry.getValue());
+            }*/
+            System.out.println(end - start);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        char[] text = readTextFromFile();
+        int n_grams = 3;
+
+        //startSequential(text,n_grams);
+        startParallel(text,n_grams);
     }
 }
