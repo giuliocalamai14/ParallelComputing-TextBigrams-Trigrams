8edc71fe335905444a18ed56ea6590ec5bbfb7f1
Marco Loschiavo <marcolos04@gmail.com>
2019-06-28 16:14:46 +0200

    Final

--- Java/Parallel/Main.java
+++ Java/Parallel/Main.java
@@ -34,7 +34,10 @@ public class Main {
 
 
     public static ConcurrentHashMap<String, Integer> HashMerge(ConcurrentHashMap<String, Integer> n_grams, ConcurrentHashMap<String, Integer> finalNgrams) {
-    // input: n_grams = portion of text executed by threads ; finalNgrams: final HashMap
+    // Marge n_grams in finalNgrams
+
+        //long s, e;
+        //s = System.currentTimeMillis();
         for (ConcurrentHashMap.Entry<String, Integer> entry : n_grams.entrySet()) {
             int newValue = entry.getValue();
             Integer existingValue = finalNgrams.get(entry.getKey());
@@ -43,6 +46,11 @@ public class Main {
             }
             finalNgrams.put(entry.getKey(), newValue);
         }
+
+        // e = System.currentTimeMillis();
+        // System.out.println(e - s);
+        // Tested that all HashMerge executes in 10-12 ms (average) in a total of 10 sec for the 150Mb txt
+
         return finalNgrams;
     }
 
@@ -67,20 +75,20 @@ public class Main {
         int fileLen = text.length;  // save text length
 
         ConcurrentHashMap<String, Integer> finalNgrams = new ConcurrentHashMap(); // Create ConcurrentHashMap cointaining <gram, value>
-        ArrayList<Future> futuresArray = new ArrayList<>();  // Create an ArrayList of Future that will contain the threads
-        ExecutorService executor = Executors.newFixedThreadPool(n_thread);
+        ArrayList<Future> futuresArray = new ArrayList<>();  // Create an ArrayList of Future that will contain the threads results
+        ExecutorService executor = Executors.newFixedThreadPool(n_thread); // create the threads
 
         long start, end;
         start = System.currentTimeMillis();
 
         double k = Math.floor(fileLen/n_thread); // divide text according to number of threads
         for (int i = 0; i < n_thread; i++) {
-            Future f = executor.submit(new Parallel_thread("t" + i, i * k, ((i+1) * k) + (n_g - 1) - 1, n_g, text));
+            Future f = executor.submit(new Parallel_thread("t" + i, i * k, ((i+1) * k) + (n_g - 1) - 1, n_g, text)); //executor.submit start the Callable call that save the results in a Future
             futuresArray.add(f);
         }
         try{
             for (Future <ConcurrentHashMap<String, Integer>> f : futuresArray) {
-                ConcurrentHashMap<String, Integer> n_grams = f.get();
+                ConcurrentHashMap<String, Integer> n_grams = f.get();  // f.get return the thread result
                 HashMerge(n_grams,finalNgrams);
             }
             awaitTerminationAfterShutdown(executor);
